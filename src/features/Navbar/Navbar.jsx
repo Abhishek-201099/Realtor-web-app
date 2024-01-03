17362d08e3e953a5787b486c91c2deffd2283726
Abhishek Bhattacharjee <ahishek1250@gmail.com>
2024-01-03 21:20:32 +0530

    Implemented header

--- src/features/Navbar/Navbar.jsx
+++ src/features/Navbar/Navbar.jsx
@@ -1,3 +1,23 @@
+import { NavLink, useNavigate } from "react-router-dom";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  const navigate = useNavigate();
+  return (
+    <header className="navbar">
+      <div className="nav-logo-container" onClick={() => navigate("/")}>
+        <img className="nav-logo" src="/logo.png" alt="realtor website logo" />
+      </div>
+      <ul className="nav-list">
+        <li className="nav-list-item">
+          <NavLink to="/home">Home</NavLink>
+        </li>
+        <li className="nav-list-item">
+          <NavLink to="/offers">Offers</NavLink>
+        </li>
+        <li className="nav-list-item">
+          <NavLink to="/sign-in">SignIn</NavLink>
+        </li>
+      </ul>
+    </header>
+  );
 }
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -3,7 +3,7 @@ import Navbar from "../features/Navbar/Navbar";
 
 export default function AppLayout() {
   return (
-    <div>
+    <div className="container">
       <Navbar />
       <Outlet />
     </div>
