9e44ac9ae14c1ee17f24b4885d661c3f659dceaa
Abhishek Bhattacharjee <ahishek1250@gmail.com>
2024-01-15 20:58:25 +0530

    Added listing details and styles

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -2,8 +2,12 @@ import { doc, getDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { db } from "../firebase";
+import { FaBed, FaBath, FaMapMarkerAlt } from "react-icons/fa";
+import { LuParkingCircle, LuParkingCircleOff } from "react-icons/lu";
+import { TbArmchair, TbArmchairOff } from "react-icons/tb";
 import Loader from "../ui/Loader";
 import ImageSlider from "../ui/ImageSlider";
+import { capitalizeEachWord } from "../helpers/helpers";
 
 export default function Listing() {
   const params = useParams();
@@ -45,7 +49,69 @@ export default function Listing() {
         <div className="listing-imgs">
           <ImageSlider imgUrls={listingData?.imgUrls} />
         </div>
-        <div className="listing-details"></div>
+        <div className="listing-details">
+          <p className="listing-name">
+            {capitalizeEachWord(listingData?.name)}
+          </p>
+          <p className="listing-address">
+            <span>
+              <FaMapMarkerAlt />
+            </span>
+            <span>{listingData?.address}</span>
+          </p>
+          <p className="listing-price">
+            &#8377;{" "}
+            {listingData?.offer === "yes"
+              ? listingData?.discountPrice?.toLocaleString("en-IN")
+              : listingData?.regularPrice?.toLocaleString("en-IN")}
+            {listingData?.offer === "yes" ? "/ month" : ""}
+          </p>
+          <p className="listing-description">{listingData?.description}</p>
+          <div className="listing-amenities">
+            <p className="listing-amenities-item">
+              <span>
+                <FaBath />
+              </span>
+              <span>
+                {listingData?.baths} {listingData?.baths > 1 ? "Baths" : "Bath"}
+              </span>
+            </p>
+            <p className="listing-amenities-item">
+              <span>
+                <FaBed />
+              </span>
+              <span>
+                {listingData?.beds} {listingData?.beds > 1 ? "Beds" : "Bed"}
+              </span>
+            </p>
+            <p className="listing-amenities-item">
+              <span>
+                {listingData?.parkingSpot === "yes" ? (
+                  <LuParkingCircle />
+                ) : (
+                  <LuParkingCircleOff />
+                )}
+              </span>
+              <span>
+                {listingData?.parkingSpot === "yes" ? "Parking" : "No parking"}
+              </span>
+            </p>
+            <p className="listing-amenities-item">
+              <span>
+                {listingData?.furnished === "yes" ? (
+                  <TbArmchair />
+                ) : (
+                  <TbArmchairOff />
+                )}
+              </span>
+              <span>
+                {listingData?.furnished === "yes"
+                  ? "Furnished"
+                  : "Not furnished"}
+              </span>
+            </p>
+          </div>
+        </div>
       </div>
       <div className="listing-map"></div>
     </section>
